34f2fa5b190e8af9ceecdafb8fbd712231d2009a
Cliff-Ezra <cliffezra212@gmail.com>
2024-01-10 21:36:43 +0300

    `feat:` change column names and json

--- app/[theme]/home/components/columns.tsx
+++ app/[theme]/home/components/columns.tsx
@@ -35,7 +35,7 @@ export const columns: ColumnDef<Task>[] = [
   {
     accessorKey: "id",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Task" />
+      <DataTableColumnHeader column={column} title="Log" />
     ),
     cell: ({ row }) => <div className="w-[80px]">{row.getValue("id")}</div>,
     enableSorting: false,
@@ -44,7 +44,7 @@ export const columns: ColumnDef<Task>[] = [
   {
     accessorKey: "title",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Title" />
+      <DataTableColumnHeader column={column} title="Description" />
     ),
     cell: ({ row }) => {
       const label = labels.find((label) => label.value === row.original.label)
