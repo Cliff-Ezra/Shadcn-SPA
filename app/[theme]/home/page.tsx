ee0609f5aa9668d5f93871a8222260c1694ef4d4
Cliff-Ezra <cliffezra212@gmail.com>
2024-01-10 19:11:59 +0300

    `refactor:` change table text

--- app/[theme]/home/page.tsx
+++ app/[theme]/home/page.tsx
@@ -10,7 +10,6 @@ import { Tabs, TabsContent } from "@/components/ui/tabs"
 
 import { columns } from "./components/columns"
 import { DataTable } from "./components/data-table"
-import { UserNav } from "./components/user-nav"
 import { taskSchema } from "./data/schema"
 
 export const metadata: Metadata = {
@@ -110,14 +109,8 @@ export default async function HomePage() {
                 <div className="flex items-center justify-between space-y-2">
                   <div>
                     <h2 className="text-2xl font-bold tracking-tight">
-                      Welcome back!
+                      Here are your logs!
                     </h2>
-                    <p className="text-muted-foreground">
-                      Here&apos;s a list of your tasks for this month!
-                    </p>
-                  </div>
-                  <div className="flex items-center space-x-2">
-                    <UserNav />
                   </div>
                 </div>
                 <DataTable data={tasks} columns={columns} />
