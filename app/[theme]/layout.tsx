6db884b29b0b3daac2d236b7c7835e6b9b433adb
Cliff-Ezra <cliffezra212@gmail.com>
2024-01-29 23:01:15 +0300

    `refactor:`remove unused components and data files

--- app/[theme]/layout.tsx
+++ app/[theme]/layout.tsx
@@ -8,16 +8,11 @@ import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn, sanitizeName } from "@/lib/utils"
 import { Toaster } from "@/components/ui/toaster"
-import { ExamplesNav } from "@/components/examples-nav"
 import Footer from "@/components/footer"
 import PageHeader from "@/components/home/pageHeader"
 import { ShadcnThemeProvider } from "@/components/shadcn-theme-provider"
 import { SiteHeader } from "@/components/site-header"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
-import { ThemeArrows } from "@/components/theme-arrows"
 import { ThemeProvider } from "@/components/theme-provider"
-
-import PHProvider from "./providers"
 
 export async function generateMetadata(params: {
   params: { theme: string }
@@ -70,39 +65,34 @@ export default function RootLayout({ children, params }: RootLayoutProps) {
       className={`theme-${params.theme}`}
     >
       <head />
-      <PHProvider>
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-          suppressHydrationWarning={true}
-        >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <ShadcnThemeProvider>
-              <ThemeArrows params={params}>
-                <div className="flex min-h-screen flex-col">
-                  <SiteHeader />
-                  <div className="flex-1">
-                    <div className="container pb-10">
-                      <PageHeader theme={params.theme} />
-                      <ExamplesNav />
-                      <section className="block">
-                        <div className="overflow-hidden rounded-lg border bg-background shadow-xl">
-                          {children}
-                        </div>
-                      </section>
-                    </div>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+        suppressHydrationWarning={true}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ShadcnThemeProvider>
+              <div className="flex min-h-screen flex-col">
+                <SiteHeader />
+                <div className="flex-1">
+                  <div className="container pb-10">
+                    <PageHeader theme={params.theme} />
+                    <section className="block">
+                      <div className="overflow-hidden rounded-lg border bg-background shadow-xl">
+                        {children}
+                      </div>
+                    </section>
                   </div>
-                  <Footer />
                 </div>
-                <Toaster />
-              </ThemeArrows>
-              <TailwindIndicator />
-            </ShadcnThemeProvider>
-          </ThemeProvider>
-        </body>
-      </PHProvider>
+                <Footer />
+              </div>
+              <Toaster />
+
+          </ShadcnThemeProvider>
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,17 +1,6 @@
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu"
 
 export function SiteHeader() {
   return (
