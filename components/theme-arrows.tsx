d41601961711c63d9597fb195c4daf9f9bf8f421
Cliff-Ezra <cliffezra212@gmail.com>
2024-01-29 19:56:55 +0300

    `refactor:` remove theme arrows navigation

--- components/theme-arrows.tsx
+++ components/theme-arrows.tsx
@@ -23,15 +23,5 @@ export function ThemeArrows({
 
   const currentPage = nested ? `${nested.join("/")}` : ""
 
-  return (
-    <div>
-      {/* <Link href={`/${previousTheme}/${currentPage}`}>
-        <ChevronLeft className="fixed left-0 top-1/2 h-10 w-10 cursor-pointer text-primary md:h-20 md:w-20" />
-      </Link>
-      <Link href={`/${nextTheme}/${currentPage}`}>
-        <ChevronRight className="fixed right-0 top-1/2 h-10 w-10 cursor-pointer text-primary md:h-20 md:w-20" />
-      </Link> */}
-      {children}
-    </div>
-  )
+  return <div>{children}</div>
 }
